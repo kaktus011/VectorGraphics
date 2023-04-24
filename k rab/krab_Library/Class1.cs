aba910d18e99fb9ddb20f238470512f5736a7681
Denis _ <piwkillpie@gmail.com>
2023-04-24 21:21:00 +0300

    _

--- k rab/krab_Library/Class1.cs
+++ k rab/krab_Library/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,6 @@ namespace krab_Library
 {
     public class Class1
     {
+        
     }
 }
